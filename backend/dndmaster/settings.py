fd85eadc553cbb37ba5be02809ebedf90a3b8ea4
Sidorov Andrey <andrey.sidorov@corp.mail.ru>
2020-08-27 17:17:22 +0300

    Реализовал сборку

--- backend/dndmaster/settings.py
+++ backend/dndmaster/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-l$4a-g5rz=#vo^s@t=q$3vei41m#3das4(kx&k))7du%*hvwo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", ]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
@@ -113,8 +119,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+# URL для отображения в Swagger
+API_URL = os.getenv("API_URL", None)
+
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- backend/dndmaster/urls.py
+++ backend/dndmaster/urls.py
@@ -14,8 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.conf import settings
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Dictionaries API",
+        default_version='v1',
+        description="OpenAPI specification for dictionaries",
+        contact=openapi.Contact(email="p.velizhansky@corp.mail.ru"),
+    ),
+    public=True,
+    url=settings.API_URL,
+    authentication_classes=(),
+    permission_classes=(),
+)
+
+urlpatterns += [
+    re_path('^swagger(?P<format>.json|.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+
